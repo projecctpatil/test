460816979641d5a45bba063575e43c6d6bb25b72
konrohit <rohit@konnectgen.com>
2021-12-23 11:48:21 +0530

    restore

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -18,20 +18,80 @@ class Welcome extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
-	public function index(){
-		if ($this->session->userdata('username')){
-			// echo 'yes';
-		}else{
+	public function __construct(){
+		parent::__construct();
+	}
+
+	public function logout(){
+		$this->session->unset_userdata('username');
+		redirect('');
+	} 
+
+	public function login(){
+		$this->load->model('Home_model');
+		$result = $this->Home_model->checkLogin($this->input->post('email') , $this->input->post('password'));
+		if ($result) {
+			$this->session->set_userdata('username',$this->input->post('email'));
+			redirect('Home');
+		} else {
+			$this->session->set_flashdata('login','Please type right user id and password !');
 			redirect('logout');
 		}
+	}
+
+	public function company_login(){
 		$this->load->model('Home_model');
-		$walkon = $this->Home_model->walkon_count();
-		$stepon = $this->Home_model->stepon_count();
-		$pedalon = $this->Home_model->pedalon_count();
-		$this->load->view('welcome_message',['walkon'=>$walkon,'stepon'=>$stepon,'pedalon'=>$pedalon]);
+		$result = $this->Home_model->checkLogin($this->input->post('email') , $this->input->post('password'));
+		if ($result) {
+			$this->session->set_userdata('username',$this->input->post('email'));
+			redirect('Home');
+		} else {
+			$this->session->set_flashdata('login','Please type right user id and password !');
+			redirect('logout');
+		}
+	}
+
+	public function register(){
+		$this->load->model('Home_model');
+		$this->load->helper('string');
+		$user_name = random_string('alnum',10);
+		$password = random_string('alpha',10);
+		$data = [
+			'name' => $this->input->post('c_name'),
+			'username' => $user_name,
+			'password' => $password,
+			'admin_ids' => $user_name . ' | ' . $password . ' | ' . $this->input->post('p_number') . ' | ' .$this->input->post('email'),
+		];
+		
+		$result = $this->Home_model->create_employers($data);
+		if ($result) {
+			$this->load->config('email');
+	        $this->load->library('email');
+	        
+	        $from = $this->config->item('smtp_user');
+	        $to = $this->input->post('email');
+	        $subject = 'Your id password !';
+	        $message = 'User name : ' . $user_name . ' Password : ' . $password;
+
+	        $this->email->set_newline("\r\n");
+	        $this->email->from($from);
+	        $this->email->to($to);
+	        $this->email->subject($subject);
+	        $this->email->message($message);
+
+	        if ($this->email->send()) {
+	        	$this->session->set_flashdata('login','Please check your email for user id and password !');
+	            redirect('logout');
+	        } else {
+	            show_error($this->email->print_debugger());
+	        }
+		} else {
+			echo 'No';
+		}
+
 	}
 
 	public function test(){
-		echo "Hello Rohit";
+		$this->load->view('register');
 	}
 }
